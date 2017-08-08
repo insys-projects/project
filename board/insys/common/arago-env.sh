86569c1b69a6cb20531b8b149dff38e93f1b03b7
Karakozov Vladimir <karakozov@gmail.com>
2017-08-08 15:27:50 +0300

    Fix errors in post building scripts. Add debug output (commented now).

--- board/insys/common/arago-env.sh
+++ board/insys/common/arago-env.sh
@@ -1,8 +1,41 @@
 #!/bin/sh
 
-PWD=${HOME}/project/${BOARD}
+#echo "================================================"
+#echo " ARAGO ENV ARGS:"
+#echo " ARG0 = $0"
+#echo " ARG1 = $1"
+#echo " ARG2 = $2"
+#echo " ARGS = $#"
+#echo "================================================"
 
-export TOOLCHAIN_PATH=${PWD}/host/opt/ext-toolchain/bin
+SETTINGS_FILE='arago-env.sh'
+SCRIPT_LOC="./"
+
+if [ $# != 0 ]; then
+        SCRIPT_LOC="$1"
+else
+        #  SCRIPT_LOC should point to script location
+        if [ "$0" == "ksh" ]; then
+                SCRIPT_LOC_TMP_UNI=`readlink -f ${BRD_ARG_}`
+        else
+                SCRIPT_LOC_TMP_UNI=$BASH_SOURCE
+        fi
+        SCRIPT_LOC_TMP_UNI=${SCRIPT_LOC_TMP_UNI%/*}
+        if [ "$SCRIPT_LOC_TMP_UNI" != "" ]; then
+                if [ "$SCRIPT_LOC_TMP_UNI" == "arago-env.sh" ]; then
+                        SCRIPT_LOC_TMP_UNI="./"
+                fi
+                SCRIPT_LOC_TMP_UNI=`readlink -f ${SCRIPT_LOC_TMP_UNI}`
+                if [ $? == 0 ]; then
+                        SCRIPT_LOC=${SCRIPT_LOC_TMP_UNI}
+                fi
+        fi
+        unset SCRIPT_LOC_TMP_UNI
+fi
+
+export TOOLS_DIR=`readlink -f "${SCRIPT_LOC}"`
+
+export TOOLCHAIN_PATH=${TOOLS_DIR}/host/opt/ext-toolchain/bin
 export TARGET_SYS=arm-arago-linux-gnueabi
 export CC=${TARGET_SYS}-gcc
 export CPP="${TARGET_SYS}-gcc"
@@ -20,12 +53,12 @@ export PATH=${HOME}/embedded/common:${PATH}
 export PATH=${TOOLCHAIN_PATH}:${PATH}
 export CROSS_COMPILE=${TARGET_SYS}-
 export ARCH=arm
-export KERNELDIR=${PWD}/build/linux-master
+export KERNELDIR=${TOOLS_DIR}/build/linux-master
 export INSTALL_MOD_PATH=${HOME}/targetfs-${BOARD}
 export CSTOOL_DIR=${TOOLCHAIN_PATH}
 export CSTOOL_PREFIX=${TARGET_SYS}-
 export INSTALL_PREFIX=${HOME}/targetfs-${BOARD}
 export TARGETFS_USER=embedded
-export BUILD_DIR=${PWD}/build
+export BUILD_DIR=${TOOLS_DIR}/build
 
 export PS1="\[\e[32;1m\][arago-${BOARD}]\[\e[0m\]:\w> "
--- board/insys/common/build-bardy.sh
+++ board/insys/common/build-bardy.sh
@@ -2,25 +2,33 @@
 
 export BOARD=$2
 
-echo "================================================"
-echo "  Build BARDY for ${BOARD}"
-echo "================================================"
+#echo "================================================"
+#echo " BUILD BARY ARGS:"
+#echo " ARG0 = $0"
+#echo " ARG1 = $1"
+#echo " ARG2 = $2"
+#echo " ARGS = $#"
+#echo "================================================"
 
-cd ${HOME}/project/board/insys/common
-source ./arago-env.sh
+SCRIPT_PATH=`dirname $0`
+PROJECT_PATH=`dirname $1`
+
+#echo " SCRIPT_PATH = $SCRIPT_PATH"
+#echo " PROJECT_PATH = $PROJECT_PATH"
+
+echo "================================================"
+echo "  Build BARDY for $BOARD "
+echo "================================================"
 
 echo "====  source arago-env.sh   ===="
-
-cd ${HOME}/bardy
-
+source $SCRIPT_PATH/arago-env.sh $PROJECT_PATH
 echo "====  Ok   ===="
 
 echo "====  source Bardyenv.sh   ===="
-
-source ./BardyEnv.sh
-
+source ${HOME}/bardy/BardyEnv.sh ${HOME}/bardy
 echo "====  Ok   ===="
 
+cd ${HOME}/bardy
 make clean
 make
 
